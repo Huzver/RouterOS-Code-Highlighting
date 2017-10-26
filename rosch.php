db0d9a08cdf92f254a38c1a42d69a68edf410bc5
Huzver <huzver@yandex.ru>
2017-10-26 17:29:43 +0300

    First Version
    First version RouterOS Code Highlighting

--- rosch.php
+++ rosch.php
@@ -3,7 +3,7 @@
 Plugin Name: RouterOS Code Highlighting
 Plugin URI: http://gregory-gost.ru/
 Description: Подсветка кода для MikroTik RouterOS / Code highlighting for MikroTik RouterOS
-Version: 1.0.0
+Version: 1.0.4
 Author: Gregory (Gost)
 Author URI: http://gregory-gost.ru/
 License: MIT
@@ -34,13 +34,33 @@ function roscode($atts, $content = null) {
 	if (!$content) {
 		return '';
 	} else {
-		return '<div class="roscode">'.do_shortcode($content).'</div>';
+		// Строки поиска
+		$search = array(
+			'/(\/system|\/interface|wireless)[\ ]|(\:)/si', // Входное меню
+			'/\b(add|set|get|find|put|print)\b/si', // Действия
+			'/(default\-name|name|mtu|l2mtu|mac-address|arp|interface\-type|mode|ssid|frequency|band|channel\-width|scan-list|wireless\-protocol|vlan\-mode|vlan\-id|wds\-mode|wds\-default\-bridge|wds\-ignore\-ssid|bridge\-mode|default\-authentication|default\-forwarding|default\-ap\-tx\-limit|default\-client\-tx\-limit|hide\-ssid|security\-profile|compression)[\=]|(detail)/si', // Параметры
+			'/([\[|\]])|(\;)/si', // Символы
+		);
+		// Замена входного меню
+		$replace = array(
+			'<span class="color-menu">\\0</span>', // Входное меню
+			'<span class="color-action">\\0</span>', // Действия
+			'<span class="color-param">\\0</span>', // Параметры
+			'<span class="color-symbol">\\0</span>', // Символы
+		);
+		
+		// Обрабатываем содержание ББ кода
+		$content = str_replace("&#187;", chr(34), $content);
+		$content = preg_replace($search, $replace, $content);
+		return '<div class="roscode">'.do_shortcode($content, true).'</div>';
 	}
 }
 add_shortcode( 'RC', 'roscode' );
 
 function roscode_css(){
-	wp_enqueue_style( 'rosch', plugins_url( 'style.css', __FILE__ ) );
+	// Регистрируем файл стилей в системе
+	wp_register_style( 'rosch', plugins_url( 'style.css', __FILE__ ), array(), '104', 'all' );
+	wp_enqueue_style( 'rosch' );
 }
-add_action('wp_enqueue_scripts', 'roscode_css');
+add_action( 'wp_enqueue_scripts', 'roscode_css' );
 ?>
