b28b26896317a45e63a0066c7083d5ec2099e28f
Huzver <huzver@yandex.ru>
2017-10-30 18:27:52 +0300

    Version 1.0.9

--- rosch.php
+++ rosch.php
@@ -3,7 +3,7 @@
 Plugin Name: RouterOS Code Highlighting
 Plugin URI: http://gregory-gost.ru/
 Description: Подсветка кода для MikroTik RouterOS / Code highlighting for MikroTik RouterOS
-Version: 1.0.6
+Version: 1.0.9
 Author: Gregory (Gost)
 Author URI: http://gregory-gost.ru/
 License: MIT
@@ -35,14 +35,19 @@ function roscode($atts, $content = null) {
 		return '';
 	} else {
 		// Определяем не пусты ли переменные
-		if (get_option('input_menu') != "") {
+		if (get_option('start_command') != "") {
 			// Обрабатываем
+			$inputTerm = preg_quote(get_option('start_command'));
 			$inputTerm = trim($inputTerm);
 			$inputTerm = preg_replace("/  +/"," ",$inputTerm); // Заменить все повторяющиеся пробелы на один
-			$inputTerm = preg_quote(get_option('input_menu'));
 			$inputTerm = str_replace(chr(32), chr(124), $inputTerm);
 		} else {
-			$inputTerm = 'system';
+			// Получаем дефолтные команды
+			update_option( 'start_command', file_get_contents(plugins_url( '/syntax_default/start_command.txt', __FILE__ )), 'no' );
+			$inputTerm = preg_quote(get_option('start_command'));
+			$inputTerm = trim($inputTerm);
+			$inputTerm = preg_replace("/  +/"," ",$inputTerm); // Заменить все повторяющиеся пробелы на один
+			$inputTerm = str_replace(chr(32), chr(124), $inputTerm);
 		}
 		if (get_option('commands') != "") {
 			// Обрабатываем
@@ -83,23 +88,37 @@ function roscode($atts, $content = null) {
 		
 		// Строки поиска
 		$search = array(
-			'/([\:])('.$commandTerm.')|[\/]('.$inputTerm.')[\s]|\b('.$inputTerm.')\b/ui', // Меню и под-меню
-			'/\b('.$commandTerm.')\b/ui', // Команды
-			'/\b('.$specCommandTerm.')\b/ui', // Специальные команды
-			'/('.$parameterTerm.')[\=]/ui', // Параметры
-			'/([\[|\]])|(\;)/si', // Символы
+			'/(\:)(?![A-Z0-9]{2})/ux', // Вхождения в стартовые команды
+			'/\b(?<![\-\=\"])('.$inputTerm.')(?![\-\=\"])\b/ux', // Стартовые команды
+			//'/\b('.$commandTerm.')\b/ui', // Команды
+			//'/\b('.$specCommandTerm.')\b/ui', // Специальные команды
+			//'/('.$parameterTerm.')[\=]/ui', // Параметры
+			//'/([\[|\]])|(\;)/si', // Символы
+			//'/\\\\|\\n|\\r|\\t|\\\?|\\_|\\a|\\b|\\f|\\v|\\\h\h/ux', // Строки перехода
+			//'/#.*(?![^\<])/ux', // Комментарии
+			'/(\$|\$\")([0-9a-zA-Z]+)/ux', // Переменные
+			'/(\$)/ux', // Вхождения в стартовые команды
 		);
 		// Замена входного меню
 		$replace = array(
-			'<span class="color-menu">$0</span>', // Меню и под-меню
-			'<span class="color-action">$0</span>', // Команды
-			'<span class="color-param">$0</span>', // Специальные команды
-			'<span class="color-param">$0</span>', // Параметры
-			'<span class="color-symbol">$0</span>', // Символы
+			'<span class="color-menu">$1</span>', // Вхождения в стартовые команды
+			'<span class="color-menu">$0</span>', // Стартовые команды
+			//'<span class="color-action">$0</span>', // Команды
+			//'<span class="color-param">$0</span>', // Специальные команды
+			//'<span class="color-param">$0</span>', // Параметры
+			//'<span class="color-symbol">$0</span>', // Символы
+			//'<span class="color-escape">$0</span>', // Строки перехода
+			//'<span class="color-comment">$0</span>', // Комментарии
+			'$1<span class="color-variables">$2</span>', // Переменные
+			'<span class="color-menu">$1</span>', // Вхождения в стартовые команды
 		);
 		
-		// Обрабатываем содержание ББ кода
+		// Подготовка кода
+		$content = str_replace("<p>", "", $content); // Удаляем тег <p>
+		$content = str_replace("</p>", "<br /><br />", $content); // Удаляем тег </p>
 		$content = str_replace("&#187;", chr(34), $content); // Кавычки при копировании
+		$content = str_replace("&#171;", chr(34), $content); // Кавычки при копировании
+		// Выполняем замену
 		$content = preg_replace($search, $replace, $content);
 		return '<div class="roscode">'.do_shortcode($content, true).'</div>';
 	}
@@ -108,7 +127,7 @@ add_shortcode( 'RC', 'roscode' );
 
 // Регистрируем файл стилей в системе
 function roscode_css(){
-	wp_register_style( 'rosch', plugins_url( 'style.css', __FILE__ ), array(), '106', 'all' );
+	wp_register_style( 'rosch', plugins_url( 'style.css', __FILE__ ), array(), '109', 'all' );
 	wp_enqueue_style( 'rosch' );
 }
 add_action( 'wp_enqueue_scripts', 'roscode_css' );
@@ -128,7 +147,7 @@ function add_rosch_menu() {
 }
 // Регистрация настроек
 function register_settings() {
-	register_setting( 'rosch-settings-group', 'input_menu' );
+	register_setting( 'rosch-settings-group', 'start_command' );
 	register_setting( 'rosch-settings-group', 'commands' );
 	register_setting( 'rosch-settings-group', 'specialcommands' );
 	register_setting( 'rosch-settings-group', 'parameters' );
@@ -137,16 +156,26 @@ function register_settings() {
 // Вывод страницы настроек
 function page_settings_generate() {
 	global $pageTitle;
+	if (get_option('start_command') != "") {
+		$inputMenu = get_option('start_command');
+	} else {
+		$inputMenu = file_get_contents(plugins_url( '/syntax_default/start_command.txt', __FILE__ ));
+	}
 	?>
 	<div class="wrap">
 		<h2><?php echo $pageTitle; ?></h2>
-		<p>Все команды необходимо вводить через пробел</p>
+		<p>Все команды должны быть записаны исключительно через пробел</p>
 		<form method="post" action="options.php">
 			<?php settings_fields( 'rosch-settings-group' ); ?>
 			<table class="form-table">
 				<tr valign="top">
-					<th scope="row">Меню и под-меню<br /><span style="color:rgb(0,155,155);">Цвет в терминале</span></th>
-					<td><textarea style="width:100%;min-height:150px;" name="input_menu"><?php echo get_option('input_menu'); ?></textarea></td>
+					<th scope="row">
+						Стартовые команды<br />
+						<span style="color:rgb(0,155,155);">Цвет в терминале</span><br />
+						Пример:<br />
+						<span style="background-color:#fff;font-family: Menlo,Monaco,Consolas,'Courier New',monospace;color:rgb(0,155,155);font-size:0.9em;">/interface wireless</span>
+					</th>
+					<td><textarea style="width:100%;min-height:150px;" name="start_command"><?php echo $inputMenu; ?></textarea></td>
 				</tr>
 
 				<tr valign="top">
@@ -155,12 +184,12 @@ function page_settings_generate() {
 				</tr>
 				
 				<tr valign="top">
-					<th scope="row">Специальные команды<br /><span style="color:rgb(0,155,0);">Цвет в терминале</span></th>
+					<th scope="row">Параметры чтение-запись<br /><span style="color:rgb(0,155,0);">Цвет в терминале</span></th>
 					<td><textarea style="width:100%;min-height:150px;" name="specialcommands"><?php echo get_option('specialcommands'); ?></textarea></td>
 				</tr>
 
 				<tr valign="top">
-					<th scope="row">Параметры<br /><span style="color:rgb(0,155,0);">Цвет в терминале</span></th>
+					<th scope="row">Параметры только чтение<br /><span style="color:rgb(0,155,0);">Цвет в терминале</span></th>
 					<td><textarea style="width:100%;min-height:150px;" name="parameters"><?php echo get_option('parameters'); ?></textarea></td>
 				</tr>
 				
